d11bde934d7c245a2b0d5e47114dafb6b41f0357
MistyRain <64189704+yaoxiaonie@users.noreply.github.com>
2021-08-30 01:25:17 +0800

    Add a percentage display

--- sync.sh
+++ sync.sh
@@ -2,10 +2,9 @@
 
 # Copyright (C) 2021 MistyRain <1740621736@qq.com>
 
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 SYNC_URL="$1"
 if [ ! -n "$2" ]; then
-    SYNC_OUT="$PROJECT_DIR/$(echo "$SYNC_URL" | sed 's|/| |g' | awk '{print $NF}')"
+    SYNC_OUT="$(echo "$SYNC_URL" | sed 's|/| |g' | awk '{print $NF}')"
 else
     SYNC_OUT="$2"
 fi
@@ -17,21 +16,29 @@ fi
 
 function download() {
     curl -sL "$1" -o "$2"
+    if [ "$?" = "1" ]; then
+        echo "接收途中发现错误，已终止！"
+        rm -rf $SYNC_OUT
+        exit 1
+    fi
 }
 
 mkdir -p $SYNC_OUT
 curl -sL "$SYNC_URL/sync.config" -o "$SYNC_OUT/sync.config"
 if [ "$?" = "1" ]; then
     echo "未发现sync.config"
+    rm -rf $SYNC_OUT
     exit 1
 fi
 
 SYNC_PROGRESS="0"
 SYNC_NUMBER=$(wc -l $SYNC_OUT/sync.config | awk '{print $1}')
+echo "接收进'${SYNC_OUT}'..."
 for SYNC_FILE in $(cat $SYNC_OUT/sync.config); do
     let SYNC_PROGRESS++
-    echo "正在拉取$SYNC_FILE （${SYNC_PROGRESS}/${SYNC_NUMBER}）"
+    echo -en "接收对象：$(echo $SYNC_PROGRESS*100/$SYNC_NUMBER | bc)%（${SYNC_PROGRESS}/${SYNC_NUMBER}）\r"
     SYNC_FILE_PDIR=$(dirname $SYNC_FILE)
     mkdir -p $SYNC_OUT/$SYNC_FILE_PDIR
     download "$SYNC_URL/$SYNC_FILE" "$SYNC_OUT/$SYNC_FILE"
 done
+echo -en "接收对象：$(echo $SYNC_PROGRESS*100/$SYNC_NUMBER | bc)%（${SYNC_PROGRESS}/${SYNC_NUMBER}），完成！\r"
